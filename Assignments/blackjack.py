d08ae97da066750d2672abcf9489fa8fbc6da8d4
Malik <mcmiki2002@gmail.com>
2018-03-03 20:15:04 +0000

    adds blackjack

---
+++ Assignments/blackjack.py
@@ -0,0 +1,38 @@
+import random
+#1.deck
+#
+class Deck:
+    def __init__(self):
+        self.deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]*4
+        random.shuffle(self.deck)
+
+    def show_deck(self):
+        print(self.deck)
+
+    def deal_card(self):
+        return self.deck.pop()
+#2.player
+class Player:
+    def __init__(self):
+        self.hand = []
+        self.score = 0
+
+    def draw(self, card):
+        self.hand.append(card)
+        self.score += card
+        self.is_busted?
+
+    def is_busted?(self):
+        if self.score > 21:
+            print("you busted")
+        else:
+            print(self.score)
+            print("you are still in the game")
+
+    def show_hand(self):
+        print(self.hand)
+        
+
+#3.init
+deck = Deck()
+deck.show_deck()
